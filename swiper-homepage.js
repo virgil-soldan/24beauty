5e685bda8dedafcfd3e6d78d505da53dffe8bfda
virgil-soldan <34655345+virgil-soldan@users.noreply.github.com>
2022-10-27 18:59:11 +0300

    Update swiper-homepage.js

--- swiper-homepage.js
+++ swiper-homepage.js
@@ -9,8 +9,8 @@ $( document ).ready(function() {
         URLhashListener:true,
         autoplayHoverPause:true,
         startPosition: '1',
-          animateOut: 'slideOutLeft',
-    animateIn: 'flipInY',
+          animateOut: 'slideOutRight',
+    animateIn: 'slideInLeft',
           smartSpeed:1000,
           
     responsive:{
