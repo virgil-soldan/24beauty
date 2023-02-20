cf2813b6231f4b072399a4b864beef5beee5b818
Virgil Soldan <virgil.soldan@gmail.com>
2023-02-20 17:57:39 +0200

    update back

--- swiper-homepage.js
+++ swiper-homepage.js
@@ -50,34 +50,4 @@ $(document).ready(function() {
    });
  });
 
-function resizedw(){
-       var w = window.matchMedia("(max-width: 640px)");
-      var vid = document.getElementById("video_piuma_el");
-      var source = document.createElement("source");
-      source.id = "hvid";
-      source.setAttribute("type", "video/mp4");
-      vid.appendChild(source);
-      
-      if (w.matches) {
-        vid.pause();
-        source.removeAttribute("src");
-        source.setAttribute("src", "https://cdn.jsdelivr.net/gh/virgil-soldan/24beauty@33ca5ba03919202ceb6084221ffc3dcde9b55ff2/piuma-stella-mobile.mp4");
-        vid.load();
-        vid.play();
-      } else {
-        vid.pause();
-        source.removeAttribute("src");
-        source.setAttribute("src", "https://cdn.jsdelivr.net/gh/virgil-soldan/24beauty@33ca5ba03919202ceb6084221ffc3dcde9b55ff2/piuma-stella-desktop.mp4");
-        vid.load();
-        vid.play();
-      }
-}
-
-var resizeHCarousel;
-window.onresize = function(){
-  clearTimeout(resizeHCarousel);
-  resizeHCarousel = setTimeout(resizedw, 500);
-};
-
-
 
