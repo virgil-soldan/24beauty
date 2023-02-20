595010e97638574fe72f5d103d8fcbcc1524dfd2
Virgil Soldan <virgil.soldan@gmail.com>
2023-02-20 16:47:45 +0200

    update

--- swiper-homepage.js
+++ swiper-homepage.js
@@ -1,25 +1,51 @@
+ $(document).ready(function() {
+            var w = window.matchMedia("(max-width: 640px)");
+      var vid = document.getElementById("video_piuma_el");
+      var source = document.createElement("source");
+      source.id = "hvid";
+      source.setAttribute("type", "video/mp4");
+      vid.appendChild(source);
+      
+      if (w.matches) {
+        vid.pause();
+        source.removeAttribute("src");
+        source.setAttribute("src", "https://uxs.ro/24beauty/piuma-stella-mobile.mp4");
+        vid.load();
+        vid.play();
+      } else {
+        vid.pause();
+        source.removeAttribute("src");
+        source.setAttribute("src", "https://uxs.ro/24beauty/piuma-stella-desktop.mp4");
+        vid.load();
+        vid.play();
+      }
+      
+   var owlTop = $('.homepage-carousel').owlCarousel({
+     margin: 0,
+     items: 1,
+     loop: false,
+     arrows:false,
+     nav: fale,
+     autoplay: true,
+     singleItem: true,
+     video: true,
+     responsive: {
+       0: {
+         items: 1
+       },
+       600: {
+         items: 1
+       },
+       1000: {
+         items: 1
+       }
+     }
+   });
 
-$( document ).ready(function() {
-      $('.homepage-carousel').owlCarousel({
-    margin:10,
-    nav:true,
-           items:1,
-        loop:true,
-        URLhashListener:true,
-        autoplayHoverPause:true,
-        startPosition: 'halloween',
-          smartSpeed:1000,
-          
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
-});
-});
+   owlTop.on('changed.owl.carousel', function(event) {
+     if ($(".owl-item.active video", this.$element)[0]) {
+       $(".owl-item.active video", this.$element)[0].play();
+     }
+
+   });
+ });
